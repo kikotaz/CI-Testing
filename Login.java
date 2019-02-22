a0adca70336d90ac7f3a698ce2ddf7695c5454ab
Karim Saleh <karim.m.saleh@hotmail.com>
2019-02-22 13:04:41 +1300

    Version 2 22/02/2019

--- Login.java
+++ Login.java
@@ -3,7 +3,7 @@ public class Login{
 	public static void main(String[] args){
 		
 		for(int i = 1; i<=10; i++){
-			System.out.println("Hello World");
+			System.out.println("Login succeeded " + i + " times");
 		}
 	}
 }
